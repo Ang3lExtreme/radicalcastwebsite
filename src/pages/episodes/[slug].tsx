fea7dc81e2e1f6efcff60920e4890dee70d91297
Frederico Neves <frednevesal@gmail.com>
2021-04-23 04:14:02 +0100

    day 2

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,96 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
+import {useRouter} from 'next/router'
+import { api } from '../../services/api';
+import {format, parseISO} from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
+import { totimeString } from '../../utils/totimeString';
+import styles from './episode.module.scss';
+import Image from 'next/image';
+import Link from 'next/link'
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  member: string;
+  duration: number;
+  durationString: string;
+  url: string;
+  publishedAt: string;
+  description: string;
+}
+
+type EpisodeProps = {
+  episode: Episode;
+}
+
+
+export default function Episode({episode}: EpisodeProps){
+
+
+  return(
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+        <button type="button">
+         <img src="/arrow-left.svg" alt="Voltar"/>
+         </button> 
+        </Link>
+       
+         <Image 
+         width={700} 
+         height={160} 
+         src={episode.thumbnail} 
+         objectFit="cover"
+         />
+         <button type="button">
+           <img src="/play.svg" alt="tocar episódio"/>
+         </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.member}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationString}</span>
+        </header>
+
+      <div 
+          className={styles.description} dangerouslySetInnerHTML={{__html: episode.description}}/>
+       
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () =>{
+  return{
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) =>{
+
+  const {slug} = ctx.params;
+  const {data} = await api.get(`/episodes/${slug}`)
+
+  const episode = {
+    id: data.id,
+      title: data.title,
+      thumbnail: data.thumbnail,
+      member: data.members,
+      publishedAt: format(parseISO(data.published_at), 'd MMM yy', {locale: ptBr}),
+      duration: Number(data.file.duration),
+      durationString: totimeString(Number(data.file.duration)),
+      description: data.description,
+      url: data.file.url,
+    };
+  
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24  //24horas
+  }
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,150 @@
+import { GetStaticProps } from "next";
 import React from "react";
 import { Header } from "../components/Header";
+import { api } from "../services/api";
+import {format, parseISO} from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR';
+import { totimeString } from "../utils/totimeString";
+import styles from "./home.module.scss";
+import Image from 'next/image';
+import Link from 'next/link';
 
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  member: string;
+  duration: number;
+  durationString: string;
+  url: string;
+  publishedAt: string;
 
-export default function Home() {
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+}
+
+export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
   return (
-    <div></div>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisode}>
+        <h2>Últimos episódios</h2>
+        <ul>
+          {latestEpisodes.map(epi =>{
+          return (
+                <li key={epi.id}>
+                  
+                    <Image 
+                      width={192} 
+                      height={192} 
+                      src={epi.thumbnail} 
+                      alt={epi.title}
+                      objectFit='contain'
+                      />
+                    <div className={styles.episodeDetails}>
+                      <Link href={`/episodes/${epi.id}`}>
+                      <a >{epi.title}</a>
+                      </Link>
+                      <p>{epi.member}</p>
+                      <span>{epi.publishedAt}</span>
+                      <span>{epi.durationString}</span>
+                    </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episodio"/>
+                </button>
+
+                </li>
+          )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr><th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th> </tr>
+                    
+          </thead>
+            <tbody>
+              {allEpisodes.map(epi =>{
+                return (
+                  <tr key={epi.id}>
+                    <td  style={{width:72}}>
+                      <Image
+                        width={120}
+                        height={120}
+                        src={epi.thumbnail}
+                        alt={epi.title}
+                        objectFit="cover"
+                        />
+                    </td>
+                    <td>
+                    <Link href={`/episodes/${epi.id}`}>
+                      <a >{epi.title}</a>
+                      </Link>
+                    </td>
+                    <td>{epi.member}</td>
+                    <td style={{width:100}}>{epi.publishedAt}</td>
+                    <td>{epi.durationString}</td>
+                    <td>
+                      <button type="button">
+                        <img 
+                        src="/play-green.svg"
+                        alt="Tocar episódio"
+                        />
+                      </button>
+                      </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+        </table>
+
+      </section>
+
+    </div>
     
   );
+}
+
+export const getStaticProps: GetStaticProps = async () =>{
+  const {data } = await api.get('episodes',{
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+  
+  const episodes = data.map(episode =>{
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      member: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBr}),
+      duration: Number(episode.file.duration),
+      durationString: totimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+    };
+  })
+
+  const latestEpisodes = episodes.slice(0,2)
+  const allEpisodes = episodes.slice(0, episodes.length)
+
+  return { 
+    props: {
+     latestEpisodes,
+     allEpisodes
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
